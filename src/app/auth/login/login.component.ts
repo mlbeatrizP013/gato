22ff27ccf8998c761ccf712c6ab9a493646f6c0c
Beatriz Patricia Martínez López <mlbeatrice013@gmail.com>
2025-05-26 20:47:46 -0600

    creacion de registerFun

--- src/app/auth/login/login.component.ts
+++ src/app/auth/login/login.component.ts
@@ -1,8 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { IonicModule } from '@ionic/angular';
-import { FormGroup } from '@angular/forms';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import {IonContent,IonInput,IonButton,IonCard,IonCardHeader,IonCardTitle,IonItem, IonLabel,IonCardContent} from '@ionic/angular/standalone';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-login',
@@ -12,8 +13,22 @@ import {IonContent,IonInput,IonButton,IonCard,IonCardHeader,IonCardTitle,IonItem
   ]
 })
 export class LoginComponent  implements OnInit {
+  registerForm: FormGroup;
 
-  constructor() { }
+   constructor(private fb: FormBuilder, private router: Router) {
+    this.registerForm = this.fb.group({
+      name: ['', Validators.required],
+      email: ['', [Validators.required, Validators.email]],
+      password: ['', Validators.required]
+    });
+  }
+   
+   register() {
+    if (this.registerForm.valid) {
+      console.log('Registro exitoso:', this.registerForm.value);
+      this.router.navigate(['/login']);
+    }
+  }
 
   ngOnInit() {}
 
